783b058cfa384c07fed96e1409099f4daac02cda
Alexander Plavin <alexander@plav.in>
2025-01-01 00:00:00 +0000

    support reading multiple files to SQLCollection

--- ext/SQLCollectionsExt.jl
+++ ext/SQLCollectionsExt.jl
@@ -10,7 +10,17 @@ function QuackIO._read_file(::Type{SQLCollection}, file, duckdb_func::String; kw
 	conn = DuckDB.DB()
     colnames = DBInterface.execute(conn, qstr) |> columntable |> keys |> collect
 	ffunc = getproperty(F.Fun, "$duckdb_func(? $(kwargs_to_db_comma(kwargs)))")
-	SQLCollection(conn, F.From(ffunc(file); columns=colnames))
+	SQLCollection(conn, F.From(ffunc(F.Lit(file)); columns=colnames))
+end
+
+# XXX: should upstream
+function F.serialize!(val::AbstractVector{<:AbstractString}, ctx)
+    print(ctx, '[')
+    for (i, v) in enumerate(val)
+        i == 1 || print(ctx, ", ")
+        F.serialize!(v, ctx)
+    end
+    print(ctx, ']')
 end
 
 end
