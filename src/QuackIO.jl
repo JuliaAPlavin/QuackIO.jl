7e463b7ec33797bd3b3c76a9177449c20c14066d
Alexander Plavin <alexander@plav.in>
2025-01-01 00:00:00 +0000

    escape passed strings, properly stringify arguments

--- src/QuackIO.jl
+++ src/QuackIO.jl
@@ -8,13 +8,13 @@ export write_table, read_csv, read_parquet
 function write_table(file, tbl; kwargs...)
     con = DBInterface.connect(DuckDB.DB)
 	DuckDB.register_table(con, tbl, "my_tbl")
-    qstr = "copy my_tbl to '$file' $(kwargs_to_db_brackets(kwargs))"
+    qstr = "copy my_tbl to '$(escape_sql_string(file))' $(kwargs_to_db_brackets(kwargs))"
     @debug "write_table query" qstr
 	DBInterface.execute(con, qstr)
 end
 
 function read_csv(fmt, file; kwargs...)
-    qstr = "select * from read_csv('$file' $(kwargs_to_db_comma(kwargs)))"
+    qstr = "select * from read_csv('$(escape_sql_string(file))' $(kwargs_to_db_comma(kwargs)))"
     @debug "read_csv query" qstr
     matf = fmt isa Function ? fmt : Tables.materializer(fmt)
     DBInterface.execute(DBInterface.connect(DuckDB.DB), qstr) |> matf
@@ -27,12 +27,22 @@ function read_parquet(fmt, file; kwargs...)
     DBInterface.execute(DBInterface.connect(DuckDB.DB), qstr) |> matf
 end
 
-kwargs_to_db_brackets(kwargs) = isempty(kwargs) ? "" : "($(kwargs_to_db(kwargs, " ")))"
-kwargs_to_db_comma(kwargs) = isempty(kwargs) ? "" : ", $(kwargs_to_db(kwargs, "="))"
+kwargs_to_db_brackets(kwargs) = isempty(kwargs) ? "" : "($(kwargs_to_db(kwargs, " ", kwarg_val_to_db_inbrackets)))"
+kwargs_to_db_comma(kwargs) = isempty(kwargs) ? "" : ", $(kwargs_to_db(kwargs, "=", kwarg_val_to_db_incomma))"
 
-kwargs_to_db(kwargs, sep) = join(["$k $sep $(kwarg_val_to_db(v))" for (k,v) in pairs(kwargs)], ", ")
-kwarg_val_to_db(x::AbstractString) = "'$x'"
+kwargs_to_db(kwargs, sep, val_to_db) = join(["$k $sep $(val_to_db(v))" for (k,v) in pairs(kwargs)], ", ")
+
+kwarg_val_to_db(x::AbstractString) = "'$(escape_sql_string(x))'"
 kwarg_val_to_db(x::Symbol) = "$x"
-kwarg_val_to_db(x) = "$x"
+kwarg_val_to_db(x::Number) = "$x"
+kwarg_val_to_db(x::Union{NamedTuple,AbstractDict}) = "{" * join(("$(kwarg_val_to_db(k)) : $(kwarg_val_to_db(v))" for (k, v) in pairs(x)), ", ") * "}"
+
+kwarg_val_to_db_inbrackets(x) = kwarg_val_to_db(x)
+kwarg_val_to_db_incomma(x) = kwarg_val_to_db(x)
+
+kwarg_val_to_db_inbrackets(x::AbstractVector) = "(" * join(kwarg_val_to_db.(x), ", ") * ")"
+kwarg_val_to_db_incomma(x::AbstractVector) = "[" * join(kwarg_val_to_db.(x), ", ") * "]"
+
+escape_sql_string(x::AbstractString) = replace(x, "'" => "''")
 
 end
